5fea6bab85a67dd2601ae17942d6b22d3c0e8a7d
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-01-19 13:37:47 +0100

    renamed data to samples

--- spreadsurfer/datacollect.py
+++ spreadsurfer/datacollect.py
@@ -1,11 +1,21 @@
 from loguru import logger
 import pandas as pd
+from fastparquet import write
+import os
 
-batch_size = 10_000
+from spreadsurfer import now
+
+dump_batch_size = 2
+data_files_path = './data'
 
 
 class DataCollector:
     def __init__(self, datacollect_queue):
+        os.makedirs(data_files_path, exist_ok=True)
+        filename = f'{now()}.parquet'
+        self.data_file = os.path.join(data_files_path, filename)
+        logger.log('data', 'writing output to {}', self.data_file)
+
         self.datacollect_queue = datacollect_queue
         self.df = pd.DataFrame(columns=['stabilized_at_ms', 'stabilized_nr_trades', 'stabilized_spread', 'wave_direction', 'last_price_delta_since_stabilized'])
 
@@ -27,3 +37,9 @@ class DataCollector:
             }]
             logger.log('data', 'wave collected: {}', fresh_data)
             self.df = pd.concat([self.df, pd.DataFrame(fresh_data)])
+
+            if len(self.df) >= dump_batch_size:
+                save_df = self.df
+                self.df = self.df.head(0)
+                append = os.path.isfile(self.data_file)
+                save_df.to_parquet(self.data_file, engine='fastparquet', append=append)
