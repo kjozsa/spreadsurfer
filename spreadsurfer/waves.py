46dcad206c7f844af856e5d4220099a0b6573d8a
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-01-19 10:21:33 +0100

    simple refactor

--- spreadsurfer/waves.py
+++ spreadsurfer/waves.py
@@ -24,21 +24,20 @@ class WaveHandler:
         self.wave = pd.DataFrame(columns=['ms' 'price', 'amount'])
         self.wave_id = None
         self.wave_stabilized = None
-        self.wave_stabilized_at_frame = None
-        self.wave_stabilized_at_price = None
+        self.wave_stabilized_frame = None
         self.wave_running = True
         self.wave_length_ms = None
 
     async def start(self):
         while True:
-            (event_name, data) = await self.wave_events_queue.get()
+            (event_name, wave_frame) = await self.wave_events_queue.get()
             match event_name:
                 case 'start':
                     self.start_wave()
                 case 'frame':
-                    await self.receive_frame(data)
+                    await self.receive_frame(wave_frame)
                 case 'end':
-                    await self.end_wave(data)
+                    await self.end_wave(wave_frame)
                 case _:
                     raise AssertionError(f'invalid event received: {event_name}')
 
@@ -53,15 +52,14 @@ class WaveHandler:
         self.wave = pd.concat([self.wave, wave_frame])
         await self.check_stabilized(wave_frame)
 
-    async def end_wave(self, data):
+    async def end_wave(self, wave_frame):
         last_wave = self.wave.tail(1)
         await self.orders_queue.put((self.wave_id, 'cancel', last_wave, None))
         self.wave_length_ms = timedelta_ms(now(), self.wave_start)
         logger.warning('ending wave {}, wave length was {} ms', self.wave_id, self.wave_length_ms)
 
         self.wave_stabilized = None
-        self.wave_stabilized_at_frame = None
-        self.wave_stabilized_at_price = None
+        self.wave_stabilized_frame = None
         self.wave_running = False
 
     async def check_stabilized(self, wave_frame):
@@ -77,21 +75,18 @@ class WaveHandler:
             wave_max_stabilized = abs(last_frames['price_max'].max() - last_frames['price_max'].mean()) < wave_stabilized_threshold
 
             if not self.wave_stabilized and not (wave_min_stabilized and wave_max_stabilized):
-                price_mean = wave_frame['price_mean'][0]
                 if wave_min_stabilized:
-                    self.wave_stabilized = "min"
-                    logger.info('wave MIN stabilized in {} ms', delta_ms)
-                    self.wave_stabilized_at_price = price_mean
-                    self.wave_stabilized_at_frame = len(self.wave)
-                    await self.orders_queue.put((self.wave_id, 'create', wave_frame, 'min'))
+                    await self.stabilized('min', delta_ms, wave_frame)
 
                 if wave_max_stabilized:
-                    self.wave_stabilized = "max"
-                    logger.info('wave MAX stabilized in {} ms', delta_ms)
-                    self.wave_stabilized_at_price = price_mean
-                    self.wave_stabilized_at_frame = len(self.wave)
-                    await self.orders_queue.put((self.wave_id, 'create', wave_frame, 'max'))
+                    await self.stabilized('max', delta_ms, wave_frame)
 
             if (self.wave_stabilized == 'min' and not wave_min_stabilized) or (self.wave_stabilized == 'max' and not wave_max_stabilized):
                 logger.log('magenta', 'WAVE IS CHANGING, cancelling orders..')
                 await self.orders_queue.put((self.wave_id, 'cancel', wave_frame, None))
+
+    async def stabilized(self, min_or_max, delta_ms, wave_frame):
+        self.wave_stabilized = min_or_max
+        logger.info('wave {} stabilized in {} ms', min_or_max.upper(), delta_ms)
+        self.wave_stabilized_frame = wave_frame
+        await self.orders_queue.put((self.wave_id, 'create', wave_frame, min_or_max))
