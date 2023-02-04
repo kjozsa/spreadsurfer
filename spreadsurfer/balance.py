9470154a530cbe4dd469b0f9edfdeaed40ba220c
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-02-04 22:12:33 +0100

    calculate profitability

--- spreadsurfer/balance.py
+++ spreadsurfer/balance.py
@@ -15,6 +15,9 @@ class BalanceWatcher:
     def __init__(self, exchange: ccxt.Exchange, connector_wss):
         self.exchange = exchange
         self.connector_wss = connector_wss
+        self.start_balance = {}
+        self.checkpoint_balance = {}
+        self.balance = {}
         self.balance_btc = None
         self.balance_usd = None
         self.last_btc_usd_rate = None
@@ -22,28 +25,41 @@ class BalanceWatcher:
 
     async def start(self):
         balance = await self.exchange.fetch_balance()
-        self.balance_btc, self.balance_usd = [float(x['free']) + float(x['locked']) for x in balance['info']['balances'] if x['asset'] in ['BTC', 'USDT']]
-        logger.info('starting balance: BTC: {}, USDT: {}', self.balance_btc, self.balance_usd)
+        self.start_balance['BTC'] = float(balance['BTC']['free']) + float(balance['BTC']['locked'])
+        self.start_balance['USDT'] = float(balance['USDT']['free']) + float(balance['USDT']['locked'])
+        logger.info('starting balance: BTC: {}, USDT: {}', self.start_balance['BTC'], self.start_balance['USDT'])
 
         while True:
             await asyncio.sleep(0)
 
             balance = await self.exchange.watch_balance()
-            self.balance_btc = float(balance['BTC']['free']) + float(balance['BTC']['locked'])
-            self.balance_usd = float(balance['USDT']['free']) + float(balance['USDT']['locked'])
+            self.balance['BTC'] = float(balance['BTC']['free']) + float(balance['BTC']['locked'])
+            self.balance['USDT'] = float(balance['USDT']['free']) + float(balance['USDT']['locked'])
             balance_total = round(self.last_btc_usd_rate * self.balance_btc + self.balance_usd, 2)
-            logger.info('total balance: {}  (BTC: {}, USDT: {}) at rate {}', balance_total, self.balance_btc, self.balance_usd, self.last_btc_usd_rate)
 
-            if balance_total < panic_below_total:
-                self.panic_countdown -= 1
-            else:
-                self.panic_countdown = panic_countdown_from
+            p = await self.calc_profitability()
+            logger.info('PROFITABILITY {} - total balance: {}  (BTC: {}, USDT: {}) at rate {}', p, balance_total, self.balance_btc, self.balance_usd, self.last_btc_usd_rate)
+            await self.check_panic_level(balance_total)
 
-            if self.panic_countdown <= 0:
-                logger.critical('total balance {} below panic level ({}), EXITING..', balance_total, panic_below_total)
-                await self.connector_wss.cancel_all_orders(f'P-{shortuuid.uuid()}')
-                await asyncio.sleep(1)
-                quit(1)
+    def calc_profitability(self):
+        # profitability == (endBTC - startBTC) * endRate + (endUSD-startUSD)   /   endBTC * endRate + endUSD
+        delta_btc = self.balance['BTC'] - self.start_balance['BTC']
+        delta_usdt = self.balance['USDT'] - self.start_balance['USDT']
+
+        n = delta_btc * self.last_btc_usd_rate + delta_usdt
+        d = self.balance['BTC'] * self.last_btc_usd_rate + self.balance['USDT']
+        return n / d
+
+    async def check_panic_level(self, balance_total):
+        if balance_total < panic_below_total:
+            self.panic_countdown -= 1
+        else:
+            self.panic_countdown = panic_countdown_from
+        if self.panic_countdown <= 0:
+            logger.critical('total balance {} below panic level ({}), EXITING..', balance_total, panic_below_total)
+            await self.connector_wss.cancel_all_orders(f'P-{shortuuid.uuid()}')
+            await asyncio.sleep(1)
+            quit(1)
 
     def sum(self, btc_usd_rate):
         return (self.balance_btc * btc_usd_rate) + self.balance_usd
