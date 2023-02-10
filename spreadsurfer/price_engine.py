f6d89f21aaf9b0eb1f31014b210f71b805600200
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-02-10 10:10:48 +0100

    log order spread

--- spreadsurfer/price_engine.py
+++ spreadsurfer/price_engine.py
@@ -97,7 +97,7 @@ class PriceEngine:
             case _:
                 raise AssertionError('missing stabilized_hint')
 
-        logger.error('wave {} - bid {}, low {}, high {}, ask {}', wave_direction, self.order_book_watcher.last_bid, low_price, high_price, self.order_book_watcher.last_ask)
+        logger.success('wave {} - bid {}, low {}, high {}, ask {} - spread {}', wave_direction, self.order_book_watcher.last_bid, low_price, high_price, self.order_book_watcher.last_ask, high_price - low_price)
 
         if low_price > high_price:
             raise Exception(f'predicted price anomaly, low_price: {low_price}, high price: {high_price}, skip placing order')
